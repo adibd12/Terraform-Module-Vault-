32e9a30b8020ecaf4b7fcc291fe92e95a843f892
adibd12 <58192161+adibd12@users.noreply.github.com>
2023-02-15 13:07:16 +0200

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,36 @@
+# variables.tf
+
+variable "kind_cluster_name" {
+  type        = string
+  description = "The name of the cluster."
+  default     = "demo-local"
+}
+
+variable "kind_cluster_config_path" {
+  type        = string
+  description = "The location where this cluster's kubeconfig will be saved to."
+  default     = "~/.kube/config"
+}
+
+variable "ingress_nginx_helm_version" {
+  type        = string
+  description = "The Helm version for the nginx ingress controller."
+  default     = "4.0.6"
+}
+
+variable "ingress_nginx_namespace" {
+  type        = string
+  description = "The nginx ingress namespace (it will be created if needed)."
+  default     = "ingress-nginx"
+}
+
+variable "METALLB_VERSION" {
+  type = string
+  default = "0.11.0"
+}
+
+variable "charts_path" {
+  default     = "./centerity-charts/infra-charts"
+  description = "The charts full path"
+}
+
